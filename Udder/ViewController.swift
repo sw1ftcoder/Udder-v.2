e76ed98560caff3cc1fc4e4aee6217c062d8f068
Jonathan Mark <jonathan.hoa@gmail.com>
2019-07-28 20:34:12 -0500

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Jonathan Mark on 7/28/19.
+//  Copyright © 2019 com.morfiend. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
